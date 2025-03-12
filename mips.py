6dd2e276f3f5cd2302c2170599554288e354afbe
Nicholas Nhat Tran <nvtran504@gmail.com>
2025-03-11 23:53:14 -0500

    mips done

--- mips.py
+++ mips.py
@@ -5,6 +5,7 @@ class MIPS_Simulator:
         self.labels = {}
         self.instructions = []
         self.file_name = file_name
+        self.current_instruction = 0
     
     def set_file_name(self, file_name):
         self.file_name = file_name
@@ -23,13 +24,21 @@ class MIPS_Simulator:
                     self.labels[label] = current_line
                     line = instruction.strip()
                     if not line:
+                        self.instructions.append(label)
                         continue
-
-                self.instructions.append(line)
+                else:
+                    self.instructions.append(line)
                 current_line += 1
 
     def execute_instruction(self, instruction):
-        parts = instruction.split()
+        label_instruction = []
+        parts = []
+        if ':' in instruction:
+            label_instruction = instruction.split(':', 1)
+            parts = label_instruction[1].split()
+        else:
+            parts = instruction.split()
+
         operation = parts[0]
         if operation == 'add':
             self.add(parts[1], parts[2], parts[3])
@@ -43,8 +52,11 @@ class MIPS_Simulator:
             self.beq(parts[1], parts[2], parts[3])
         elif operation == 'bgt':
             self.bgt(parts[1], parts[2], parts[3])
+        elif ':' in operation:
+            pass
         else:
             print(f'Unknown instruction: {operation}')
+        self.current_instruction += 1
 
     def add(self, dest, src1, src2):
        self.registers[dest] = self.registers[src1] + self.registers[src2]
@@ -53,18 +65,20 @@ class MIPS_Simulator:
         self.registers[dest] = self.registers[src1] - self.registers[src2]
 
     def lw(self, dest, src):
-        self.registers[dest] = self.memory[self.registers[src]]
+        self.registers[dest] = self.memory[src]
 
     def sw(self, src, dest):
-        self.memory[self.registers[dest]] = self.registers[src]
+        self.memory[dest] = self.registers[src]
 
     def beq(self, src1, src2, label):
         if self.registers[src1] == self.registers[src2]:
             self.execute_instruction(self.instructions[self.labels[label]])
-    
+
     def bgt(self, src1, src2, label):
+        print("something something bgt")
         if self.registers[src1] > self.registers[src2]:
             self.execute_instruction(self.instructions[self.labels[label]])
+            print(f'Jumping to {label}')
 
     def get_status(self):
         print("Registers:")
@@ -72,17 +86,20 @@ class MIPS_Simulator:
             print(f'{reg}: {self.registers[reg]}')
         print(f'Labels: {self.labels}')
         print(f'Instructions: {self.instructions}')
+        print(f'Memory: {self.memory}')
+        
 
     def run(self):
         self.parse_file()
-        for instruction in self.instructions :
+        while self.current_instruction < len(self.instructions):
+            instruction = self.instructions[self.current_instruction]
             self.execute_instruction(instruction)
-            self.get_status()
-        
-
+        print(f'Final Register State: {self.registers}')
+        print(f'Final Memory State: {self.memory}')
+            
 if __name__ == "__main__":
-    mips = MIPS_Simulator("example1.txt")
-    mips.registers["$t2"] = 6
-    mips.registers["$t3"] = 4
-    mips.get_status()
+    mips = MIPS_Simulator("example2.txt")
+    mips.registers["$t2"] = 1
+    mips.registers["$t3"] = 1
+    mips.registers["$t4"] = 1
     mips.run()
